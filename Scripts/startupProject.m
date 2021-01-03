3897579266e505f8e4b237b3fbe1bd8ba34bbe56
juan rubio <33218956+jruro@users.noreply.github.com>
2021-01-03 17:31:27 +0100

    Initial commit

---
+++ Scripts/startupProject.m
@@ -0,0 +1,10 @@
+function startupProject
+    clear
+    % clear command window
+    clc
+    % turn off all warnings
+    warning('off','all')
+    
+    %% Open instructions
+    edit Instructions.mlx
+end
---
+++ modelos/DataDictionary/Registro_variables.m
@@ -0,0 +1,42 @@
+%% USING MPT.PARAMETERS TO SET UP MY TUNABLE VARIABLES
+Boton_registro = mpt.Signal;
+Boton_registro.DataType = 'boolean';
+Boton_registro.Min = 0;
+Boton_registro.Max = 1;
+Boton_registro.Dimensions = 1;
+Boton_registro.DimensionsMode = 'fixed';
+
+Correo_valido = mpt.Signal;
+Correo_valido.DataType = 'boolean';
+Correo_valido.Min = 0;
+Correo_valido.Max = 1;
+Correo_valido.Dimensions = 1;
+Correo_valido.DimensionsMode = 'fixed';
+
+Contrase_a_valida = mpt.Signal;
+Contrase_a_valida.DataType = 'boolean';
+Contrase_a_valida.Min = 0;
+Contrase_a_valida.Max = 1;
+Contrase_a_valida.Dimensions = 1;
+Contrase_a_valida.DimensionsMode = 'fixed';
+
+Datos_validos = mpt.Signal;
+Datos_validos.DataType = 'boolean';
+Datos_validos.Min = 0;
+Datos_validos.Max = 1;
+Datos_validos.Dimensions = 1;
+Datos_validos.DimensionsMode = 'fixed';
+
+Codigo_correo = mpt.Signal;
+Codigo_correo.DataType = 'boolean';
+Codigo_correo.Min = 0;
+Codigo_correo.Max = 1;
+Codigo_correo.Dimensions = 1;
+Codigo_correo.DimensionsMode = 'fixed';
+
+registro_correcto = mpt.Signal;
+registro_correcto.DataType = 'boolean';
+registro_correcto.Min = 0;
+registro_correcto.Max = 1;
+registro_correcto.Dimensions = 1;
+registro_correcto.DimensionsMode = 'fixed';
